66930bd2dfb00af2e5ef5e45469fb938cb9acdb6
Luciana Nunes <lcnunes09@gmail.com>
2023-08-20 12:14:13 -0500

    feat: add and list tasks

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
+
+const database = new Database
+
+const server = http.createServer(async (request, response) => {
+    const { method, url } = request
+
+    await json(request, response)
+
+    if (method === 'GET' && url === '/tasks') {
+        const tasks = database.select('tasks')
+
+        return response.end(JSON.stringify(tasks))
+    }
+
+    if (method === 'POST' && url === '/tasks') {
+        const { title, description } = request.body
+
+        const task = {
+            id: 1,
+            title,
+            description,
+            completed_at: null,
+        }
+
+        database.insert('tasks', task)
+
+        return response.writeHead(201).end()
+    }
+
+    return response.writeHead(404).end()
+})
+
+server.listen(3334)
